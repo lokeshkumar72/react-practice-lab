ba05c32e5a298cfdcf0a05f9f55b3e977ebdd0c2
Lokesh kumar singh <lokeshbsr544@gmail.com>
2026-01-12 14:52:49 +0530

    Day 10: useState hook with counter app (Vite)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import Header from "./components/Header";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
-      <Header />
-      <h1>Hello React ⚛️</h1>
-      <p>Learning JSX and components</p>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>React Counter</h1>
+      <h2>{count}</h2>
+
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button
+        onClick={() => setCount(count - 1)}
+        style={{ marginLeft: "10px" }}
+      >
+        -
+      </button>
     </div>
   );
 }
