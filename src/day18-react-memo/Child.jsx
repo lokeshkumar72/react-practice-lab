b9b22761966989c6d48b917d9b2e57deae87bb90
Lokesh kumar singh <lokeshbsr544@gmail.com>
2026-01-20 15:15:40 +0530

    Day 18: Used React.memo to prevent re-renders

--- src/day18-react-memo/Child.jsx
+++ src/day18-react-memo/Child.jsx
@@ -1,4 +1,8 @@
-export default function Child() {
+import React from "react";
+
+const Child = () => {
   console.log("Child rendered");
   return <h3>I am Child Component</h3>;
-}
+};
+
+export default React.memo(Child);
