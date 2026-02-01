86bb65c27c6e0f6609b75bb9cd976c3c3ce7da9e
Lokesh kumar singh <lokeshbsr544@gmail.com>
2026-02-01 16:41:04 +0530

    Day 19: Updated App.js to render Counter component

---
+++ src/day-19-custom-hooks/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Day 19: Custom Hooks Practice</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
